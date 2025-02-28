a6022c1a3e69242a6dfb4827076dd314fac5d014
Khronos <38783502+MXKhronos@users.noreply.github.com>
2025-02-28 20:01:47 +0800

    Added Button Interaction.

--- Code/ControlScript.cs
+++ Code/ControlScript.cs
@@ -5,13 +5,41 @@ public sealed class ControlScript : Component {
 	public float cameraZoom = 100f;
 	public bool rightShoulderCamera = true;
 	public PlayerController playerController;
+	public CameraComponent playerCamera;
+	public float interactionRange = 250f;
 
 	protected override void OnStart() {
 		playerController = GetComponent<PlayerController>();
-		Log.Info("Load Control Script");
+		playerCamera = playerController.GetComponentInChildren<CameraComponent>();
+
+		playerController.StepDebug = true;
 	}
 
 	protected override void OnUpdate() {
+		Vector2 centerScreenVec = playerCamera.ScreenRect.Center;
+		Ray screenRay = playerCamera.ScreenPixelToRay(centerScreenVec);
+		SceneTraceResult traceResult = Scene.Trace.Ray(screenRay.Position, screenRay.Position + screenRay.Forward * interactionRange).Run();
+
+		DebugOverlay.ScreenText(centerScreenVec, "•");
+
+		if (traceResult.Hit) {
+			//DebugOverlay.Box(traceResult.HitPosition, new Vector3(1, 1, 1), Color.Green);
+
+			var pressable = traceResult.GameObject.GetComponent<IPressable>();
+			if (pressable != null) {
+				playerController.Hovered = traceResult.Component;
+				playerController.Pressed = playerController.Hovered;
+
+				if (Input.Pressed("use")){
+					var pressEvent = new IPressable.Event(playerController, screenRay);
+					pressable.Press(pressEvent);
+				}
+			}
+
+		} else {
+			//DebugOverlay.Box(screenRay.Position + screenRay.Forward * interactionRange, new Vector3(1, 1, 1), Color.Red);
+		}
+
 		if (Input.MouseWheel.y != 0) {
 			cameraZoom = MathX.Floor(MathX.Clamp(cameraZoom + (-Input.MouseWheel.y * 50), 0, 500));
 
--- Code/MachineButton.cs
+++ Code/MachineButton.cs
@@ -2,9 +2,13 @@ using Sandbox;
 using static Sandbox.Component;
 
 public sealed class MachineButton : Component, IPressable {
-	
 	public bool Press(IPressable.Event e) {
-		Log.Info("Pressed!");
+		var playerController = e.Source as PlayerController;
+		if (playerController == null) return false;
+
+		Log.Info("Pressed! " + playerController.Pressed);
+		var soundPoint = GetComponent<SoundPointComponent>();
+		soundPoint.StartSound();
 
 		return true;
 	}
